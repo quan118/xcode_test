5cec3fef7d402e93db69fbc5673a0a003bed0754
Quan Nguyen <quan.nguyen@niteco.se>
2018-09-21 22:29:59 +0700

    The first commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Quan Nguyen on 9/21/18.
+//  Copyright © 2018 9ecko studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
